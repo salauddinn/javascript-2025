f6e56a5e0b108593866c2cd29d17b7eb3400a94d
Salauddin <nalbandsalauddin@gmail.com>
2025-11-22 20:24:46 +0530

    intial commit

---
+++ class1/1. datatypes.js
@@ -0,0 +1,54 @@
+
+// Javascript 
+// Every sat 7 to 8/ 8:30
+// variables to promises
+
+// JavaScript
+// HTML -> render webpage 
+// CSS -> 
+// js -> intraction
+// node js -> backed
+console.log("hello world");
+/// variables
+// var (before ES6), let, const
+let a = 10;
+a=15;
+console.log(a);
+const b = 20;
+console.log(b);
+// data types
+// primitive -> number, string, boolean, null, undefined, symbol
+// non-primitive -> object, array
+// declare variable and assign value
+let num = 10; // number
+let str = "hello"; // string
+let bool = false; // boolean
+let n = null; // null
+let floatNum = 10.5; // float
+let u; // undefcined
+const pi = 3.14;
+console.log(u);
+let arr = ["a","b","c"];
+ // array
+ let obj = {name: "john", age: 30}; // object
+let listEmployee = [
+    {name: "john", age: 30},
+    {name: "doe", age: 25}
+];
+let arraylist=[[1,2,3],[3,4,5],[10,11,12]];
+
+
+let num2=100;
+console.log(num2);
+num2="hello";
+console.log(num2);
+num2=true;
+console.log(num2);
+
+// will learn about arrays
+a=[1,2,3,4,5];
+console.log(a);
+// objects
+
+
+// if else and for loop
---
+++ class1/2. arrays_and_ifelse.js
@@ -0,0 +1,106 @@
+// will learn about arraya
+// index 0,1,2,3
+let array=["apple","banana","grapes","mango","orange"];
+// console.log(array);
+// console.log(array[1]);
+// console.log(array.length);
+// console.log(array[array.length-1]);
+const OfferCODE="AI30";
+const numArray=[1,2,3,4,5];
+array=[1,4,5];
+// array=[true,false,true];
+numArray[0]=10;
+numArray[1]=20;
+numArray[2]=30;
+
+// console.log(numArray);
+array.push(6);
+
+// console.log(array);
+// console.log("popped element:");
+// console.log(array.pop());
+
+// console.log("After pop")
+// console.log(array);
+// map filter reduce once we learn functions
+
+// string
+// let message="hello";
+// meesage= ['h','e','l','l','o'];
+let msg='hello world';
+let msg2=`hello world`;
+// console.log("string length "+message.length);
+// console.log(message[0]);
+// string methods
+// console.log(message.toUpperCase()," converted to upper case");
+// console.log(message.toLowerCase()," converted to lower case");
+// console.log(message.indexOf('l'));
+// console.log(message.lastIndexOf('l'));
+// console.log(msg.slice(5,11));
+// console.log(message.replace('llo','y there'));
+// console.log(message.charAt(4));
+// typescript
+// objects
+
+// if else
+let balence=-10;
+if (balence>0){
+    console.log("balance is positive");
+}
+else if (balence==0){
+    console.log("balance is zero");
+}
+else{
+    console.log("balance is negative");
+}
+
+let str="a";
+if (str==="a"){
+    console.log("string is a");
+}
+else{
+    console.log("string is not a");
+}
+let bool=true;
+if(bool){
+    console.log("this is true");
+}else{
+    console.log("this is false");
+}
+
+// swtich case
+let day=10;
+switch(day){
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday");
+        break;
+    case 7:
+        console.log("sunday");
+        break;
+    default:
+        console.log("invalid day");
+}
+
+
+let a=13;
+if(a%2 == 0){
+    console.log("even number");
+}else{
+    console.log("odd number");
+}
+
